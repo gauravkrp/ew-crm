2c2bd7cf84fd8b012e619596aee5440b36f0fe15
Rishav Barnwal <barnwalrishav82@gmail.com>
2023-10-19 20:04:56 +0530

    data table for lead table

---
+++ src/app/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { getLeads } from "@/api/leads";
+import withLayout from "@/components/Layout";
+import axios from "axios";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+
+const columns = [
+  {
+    name: "id",
+    selector: (row: any) => row.id,
+    sortable: true,
+  },
+  {
+    name: "Name",
+    selector: (row: any) => row.name,
+  },
+  {
+    name: "Email",
+    selector: (row: any) => row.email,
+    sortable: true,
+  },
+  {
+    name: "Gender",
+    selector: (row: any) => row?.gender[0],
+  },
+  {
+    name: "Mobile",
+    selector: (row: any) => row?.mobile_number,
+  },
+  {
+    name: "Desired Course",
+    selector: (row: any) => row?.desired_course,
+    sortable: true,
+  },
+  {
+    name: "Desired Country",
+    selector: (row: any) => row?.desired_country,
+    sortable: true,
+  },
+];
+
+function Home() {
+  const [leadsData, setLeadsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
+  const [totalRows, setTotalRows] = useState(0);
+  const fetchData = async (page = 1, limit = 10) => {
+    try {
+      const {
+        data: { data, meta },
+      } = await getLeads({ page, limit });
+      setLeadsData(data);
+      setTotalRows(meta?.filter_count || 0);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePageChange = (page: number) => {
+    fetchData(page);
+    setCurrentPage(page);
+  };
+
+  const handlePerRowsChange = async (newPerPage: number, page: number) => {
+    fetchData(page, newPerPage);
+    setPerPage(newPerPage);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="md:mx-20 my-2 py-4 px-2 shadow-md rounded-lg">
+        <DataTable
+          title={"Student Leads"}
+          columns={columns}
+          data={leadsData}
+          fixedHeader={true}
+          pagination
+          paginationServer
+          paginationTotalRows={totalRows}
+          paginationDefaultPage={currentPage}
+          onChangeRowsPerPage={handlePerRowsChange}
+          onChangePage={handlePageChange}
+        />
+      </div>
+    </>
+  );
+}
+
+export default withLayout(Home);
---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect } from "react";
+import Image from "next/image";
+import MoonIcon from "@geist-ui/icons/moon";
+import SunIcon from "@geist-ui/icons/sun";
+import { Button } from "antd";
+
+import Link from "next/link";
+// import { TriggerApplyFormModalButton } from 'components/App/Common/ApplyOnlineForm';
+
+const showThemeSwitch = false;
+
+const Hamburger: React.FC<any> = ({}) => {
+  return (
+    <button
+      id="hamburger-menu"
+      onClick={() => {}}
+      className="lg:hidden block hamburger-menu navbar-toggler"
+    >
+      {/* <svg
+        xmlns="http://www.w3.org/2000/svg"
+        id="menu-button"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"
+        />
+      </svg> */}
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+const Header: React.FC<any> = ({}) => {
+  const onHamburgerClick = () => {
+    const menu = document.getElementById("menu");
+    const hamburger = document.getElementById("hamburger-menu");
+    menu?.classList.toggle("on-mobile");
+    hamburger?.classList.toggle("open");
+  };
+
+  return (
+    <header className="w-full mb-4">
+      <nav className="flex flex-wrap justify-between items-center px-4 md:px-20 py-3 shadow-sm">
+        <Link href="/">
+          <div className="w-32 relative h-14">
+            <Image src={"/assets/logo.png"} fill alt="Education World" />
+          </div>
+        </Link>
+
+        {/* <Hamburger onClick={onHamburgerClick} /> */}
+
+        <div className="w-full lg:flex lg:items-center lg:w-auto" id="menu">
+          <ul
+            className="
+              text-base text-gray-700
+              md:flex
+              md:justify-between 
+              items-center
+              md:pt-0"
+          >
+            <li>
+              <Link
+                className="nav-link md:mr-4 py-2 px-4 bg-neutral-200 rounded-lg"
+                href="https://crm.edugyanam.com/admin"
+              >
+                CRM
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="nav-link rounded-lg bg-green-600 py-2 px-4 text-white"
+                href="https://eduworldglobal.com/"
+              >
+                Our Site
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
