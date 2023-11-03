cd18ed9b2d0d0c06de56e0effc3aa5f6d6636cd9
Rishav Barnwal <barnwalrishav82@gmail.com>
2023-11-03 11:01:32 +0530

    added authentication

---
+++ src/api/leads/index.tsx
@@ -0,0 +1,24 @@
+import qs from "qs";
+import { apiInstance } from "..";
+
+const LeadsTable = "/items/student_leads";
+
+export function getLeads(params: any = {}) {
+  const { page = 1, limit = 12, filters, search } = params || {};
+  const query = qs.stringify(
+    {
+      fields: "*", // "*.*",
+      filter: {
+        // status: { _eq: "published" },
+        ...filters,
+      },
+      meta: "*",
+      limit,
+      page,
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  return apiInstance.get(`${LeadsTable}?${query}`);
+}
